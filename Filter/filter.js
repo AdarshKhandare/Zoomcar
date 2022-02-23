8a9e4b9c16e44d5c22129fafb95953779eeffe85
Dreamboy-26 <heyimpankaj2616@gmail.com>
2022-02-23 22:30:51 +0530

    filter html

---
+++ Filter/filter.js
@@ -0,0 +1,9 @@
+document.getElementById("left").addEventListener("click",change)
+
+function change(event){
+    
+    event.target.style.background = "#10a310";
+    event.target.style.color = "white";
+
+
+}
