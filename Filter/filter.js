141e9b575b41c7817d4f11b7cff19fe278ef70a0
AdarshKhandare <kingadarsh313@gmail.com>
2022-02-25 01:11:49 +0530

    none

--- Filter/filter.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("left").addEventListener("click",change)
-
-function change(event){
-    
-    event.target.style.background = "#10a310";
-    event.target.style.color = "white";
-
-
-}
