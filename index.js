714b8c5c7e72f7dd2486ed1206415cca311691fb
GovindLakhotiya <99141948+Govindlakhotiya@users.noreply.github.com>
2022-02-25 18:59:13 +0530

    confirm

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
 var signupdetails= JSON.parse(localstorage.getItem("signupDetails"));
-=======
-document.querySelector("#form").
-addEventListener("submit",myfunction);
-var toShowinFilter = JSON.parse(localStorage.getItem("myjourney")) || []
-function myfunction(event){
-    event.preventDefault()
-    var startJourney = document.querySelector("#start").value
-    var endJourney = document.querySelector("#").value
-    var pushObj = {
-        keyStart:startJourney,
-        keyEnd : endJourney,
-    };
-    toShowinFilter.push(pushObj);
-    console.log(toShowinFilter);
-    localStorage.setItem("myJourney",JSON.stringify(toShowinFilter));
-}
->>>>>>> 4c23f9c9753c795121fb6aae75b9a35451f3f418
