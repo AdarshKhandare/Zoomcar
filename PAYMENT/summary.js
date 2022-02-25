9abcb1ed6048f11a3d5b63274d1d66f0766a65b3
AdarshKhandare <kingadarsh313@gmail.com>
2022-02-26 01:13:18 +0530

    last

--- PAYMENT/summary.js
+++ PAYMENT/summary.js
@@ -8,12 +8,15 @@ showsummary(sdata)
 var div=document.createElement("div");
 
 
-
 var div1=document.createElement("div");
 div1.innerText=e.carName;
+div1.style.fontSize = "20px"
+div1.style.fontWeight = "800"
 
 var divM=document.createElement("div");
 divM.style.display="flex"
+divM.style.gap="15px"
+
 
 var div2=document.createElement("div");
 div2.innerText=e.transmission;
@@ -24,22 +27,18 @@ div3.innerText=e.fuelType;
 var div4=document.createElement("div");
 
 
-div.style.border="2px solid red";
-div.style.width="600px"
-div.style.height="100px"
-
-var para=document.createElement("p")
-para.innerText=e.carName;
 
 var img=document.createElement("img")
 img.src=e.image;
 img.style.width="100px"
-div.append(div1,divM,div4);
+
+
+div.append(div1,divM);
 divM.append(div2,div3);
 
 div4.append(img)
 // document.querySelector("div").append(para)
-document.querySelector("#parent").append(div,)
+document.querySelector("#parent").append(div,div4)
     })
  
  }
